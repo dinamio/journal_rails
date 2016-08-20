4e8bdb4e239f8ab7a8b816fc7f6303c427b06ede
Eugen Godun <egodun_ext@icontrol.com>
2016-08-20 13:01:36 +0300

    Adding change password functionality

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,11 @@ Rails.application.routes.draw do
   
 
   devise_for :users
+  resource :user, only: [:edit] do
+  collection do
+    patch 'update_password'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
